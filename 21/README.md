3f7842374530fa5905314ee77a96d37c95aa9ba1
나강민 <139530542+KangminNa@users.noreply.github.com>
2023-09-02 10:52:03 +0900

    Create : 21. CreateView를 통한 회원가입
    - CreateView import를 활용하여 간단한 모델 만들기

---
+++ 21/README.md
@@ -0,0 +1,14 @@
+# 21. CreateView를 통한 회원가입 구현
+
+## 시스템 구조
+- 도커 이내에 4개의 컨테이너를 구축
+- 장고 컨테이너를 작성하고 있는 과정(현재까지)
+![](https://github.com/KangminNa/Django_Pinterest/blob/main/21/1.png?raw=true)
+- 이 이미지에 맞춰서 구현 할 예정
+- 장고 컨테이너 코드를 구축하고 있는 과정
+
+## 실습
+- hello_world.html과 같은 디렉토리안에 create.html 생성
+- accountapp/view.py에서 CreateView import 이후에 사용
+- import 부분을 잘 살펴보고 그대로 적용해야함
+- 코드가 한결 간결해짐
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -17,4 +22,9 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-1
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
