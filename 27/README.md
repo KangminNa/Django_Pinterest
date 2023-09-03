26cc4bcf98a61725081fbff16ffea2c0759c67a3
나강민 <139530542+KangminNa@users.noreply.github.com>
2023-09-03 20:06:21 +0900

    Create : 27. Authentication 인증시스템 구축
    - 유저에 대한 부분들을 정리
    - 꼭 안해도 됨
    - 이렇게 작성하면 안되지만 설명을 위해 작성
    - 참고 이후 28강에서 다시 다 바꿀 예정

---
+++ 27/README.md
@@ -0,0 +1,11 @@
+# 27. Authentication 인증시스템 구축
+
+## 수업내용
+- 버그 수정
+- CRUD 전부 구현 완료
+- Account를 만든 이유는 Hello World에 Anyone 접근
+- Authentication을 해야함
+- 로그인한 user의 다른 아이디 접근을 막는 작업을 진행함
+- 지금은 가독성이 떨어짐
+- 이후에 가독성을 높이는 작업을 할 예정
+
--- pragmatic/accountapp/views.py
+++ pragmatic/accountapp/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -14,16 +14,18 @@ from accountapp.models import HelloWorld
 # Create your views here.
 def hello_world(request):
 
-    if request.method == "POST":
-        temp = request.POST.get('hello_world_input')
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            temp = request.POST.get('hello_world_input')
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else:
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
     else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-
+        return HttpResponseRedirect(reverse('accountapp:login'))
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
@@ -42,8 +44,29 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+    def post(self, *args, **kwargs):
+        if self.request.user.is_authenticated and self.get_object() == self.request.user:
+            return super().get(*args, **kwargs)
+        else:
+            return HttpResponseForbidden()
