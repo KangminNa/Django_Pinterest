1bb7af9a186925f14469321f37fecc2f50cc8b76
나강민 <139530542+KangminNa@users.noreply.github.com>
2023-09-06 00:00:11 +0900

    Update : window 환경에서 구축
    - 윈도우 환경에서 구축

--- pragmatic/pragmatic/settings.py
+++ pragmatic/pragmatic/settings.py
@@ -153,7 +153,7 @@ LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
 MEDIA_URL = '/media/'
 
-MEDIA_URL = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
